f54da125234b62c3fabbd9dbee931776963b4867
Gayatri Mhetre <88704532+GayatriMhetre21@users.noreply.github.com>
2025-06-18 17:18:36 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,20 @@
+//array of string
+let arr = ["gayatri", "akshay", "deepika", "virat"];
+console.log(arr);
+console.log(arr[1]);
+//mixed array
+let array = ["gayatri", 9, true, "xyz", -3];
+console.log(array);
+console.log(array[2]);
+//array in array
+let ar = ["gayatri", 7, [5, 6], "akshay"];
+console.log(ar);
+console.log(ar[2][0]);
+console.log(ar[2]);
+//nested array
+let arrr = ["gayatri", "akshay", 7, [2, 4, [5, 6, 7]]];
+console.log(arrr);
+console.log(arrr[3]);
+console.log(arrr[3][1]);
+console.log(arrr[3][3]);
+console.log(arrr[3][3][1]);
