a9c910a095e7909ad1d8290b094c634a18b68930
Gayatri Mhetre <88704532+GayatriMhetre21@users.noreply.github.com>
2025-06-18 17:21:38 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,45 @@
+//function
+function printhelloworld() {
+    console.log("hello world");
+}
+printhelloworld();
+
+function greet(name) {
+    console.log("namaste," + name);
+}
+let x = "virat";
+greet(x);
+greet("gayatri");
+
+function sum(a, b) {
+    let add = a + b;
+    console.log(add);
+}
+sum(3, 5);
+
+function summ(a, b) {
+    let add = a + b;
+    console.log(add);
+}
+let u = 10;
+let y = 20;
+let z = 30;
+
+summ(u, y);
+summ(y, z);
+summ(30, 50);
+
+function multiply(a, b) {
+    let multi = a * b;
+    console.log(multi);
+}
+multiply(10, 20);
+
+function square(t) {
+    let result = t * t;
+    console.log(result);
+}
+square(4);
+
+let value = square(3);
+console.log(value)
