947092ba7f745b5aee987c02bbc4e510036707c5
Gayatri Mhetre <88704532+GayatriMhetre21@users.noreply.github.com>
2025-06-18 17:19:52 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,21 @@
+//objects 
+let obj = {
+    a: 8,
+    name: "gayatri",
+    bool: true,
+    array: [2, 4, 6]
+}
+
+console.log(obj);
+console.log(obj.name);
+console.log(obj.array);
+
+let object = {
+    a:8,
+    firstname:"gayatri",
+    lastname:"mhetre",
+    bool:true,
+    array:[2, 4, 6]
+}
+
+console.log(object.firstname + object.lastname);
